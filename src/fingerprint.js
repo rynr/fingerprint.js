0d895e7652379822324a29da4429d79c6461763e
Rainer Jung <Rainer.Jung@gmail.com>
2010-03-21 14:41:04 +0100

    Added Handling of Plugins

--- src/fingerprint.js
+++ src/fingerprint.js
@@ -4,8 +4,9 @@
 // http://www.klauskomenda.com/code/javascript-programming-patterns/#module
 var fingerprint = function () {
 	var config = {
-		parts: ['agent'],
-		separator: '|'
+		parts: ['agent', 'plugins'],
+		separator: '|',
+		subseparator: ';'
 	}
 
 	function join_fingerprint() {
@@ -21,6 +22,35 @@ var fingerprint = function () {
 	// Retrieve UserAgent-Part
 	function fp_agent() {
 		return navigator.userAgent;
+	}
+
+	// Retrieve Plugin-Parts
+	function fp_plugins() {
+		// navigator.plugins returns a PluginArray that does not
+		// support the map function, so using for here.
+		// XXX Does someone know, how to better use map here?
+		var plugins = navigator.plugins;
+		var result = new Array(plugins.length);
+		for(i=0; i<plugins.length; i++) {
+			result[i] = extract_attributes_from_object(
+				new Array('name', 'description', 'filename'),
+				plugins[i]
+			);
+			// Also adding MimeType-Elements;
+			for(j = 0; j< plugins[i].length; j++) {
+				result[i] += extract_attributes_from_object(
+					new Array('suffixes', 'description', 'type'),
+					plugins[i][j]
+				);
+			}
+		}
+		return result.join(config.separator)
+	}
+
+	function extract_attributes_from_object(attributes, obj) {
+		return attributes.map( function(att) {
+				return eval('obj.'+ att + '.toString()')
+		}).join(config.subseparator)
 	}
 
 	return {
